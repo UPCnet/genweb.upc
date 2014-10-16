1531f19e9f3994c35d01e4f2282a0f7434821d0f
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-10-16 12:37:38 +0200

    Transferred feature viewlets from genweb.theme

--- genweb/upc/browser/viewlets.py
+++ genweb/upc/browser/viewlets.py
@@ -1,14 +1,22 @@
 from five import grok
 from plone import api
 from Acquisition import aq_inner
+from zope.annotation.interfaces import IAnnotations
+from zope.security import checkPermission
 
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 
 from plone.app.layout.viewlets.interfaces import IAboveContent
+from plone.app.layout.viewlets.interfaces import IAboveContentTitle
+from plone.app.contenttypes.interfaces import IEvent
+from plone.app.contenttypes.interfaces import INewsItem
 
+from genweb.core.adapters import IImportant
 from genweb.core.interfaces import IGenwebLayer
 from genweb.core.interfaces import IHomePage
 from genweb.core.utils import portal_url
+from genweb.theme.browser.viewlets import viewletBase
+from genweb.theme.browser.interfaces import IGenwebTheme
 
 
 class notConfigured(grok.Viewlet):
@@ -43,3 +51,48 @@ class notConfiguredForRoots(notConfigured):
     grok.template('notconfigured')
     grok.require('cmf.ManagePortal')
     grok.layer(IGenwebLayer)
+
+
+class gwSendEvent(viewletBase):
+    grok.name('genweb.sendevent')
+    grok.context(IEvent)
+    grok.template('send_event')
+    grok.viewletmanager(IAboveContentTitle)
+    grok.layer(IGenwebTheme)
+
+    def isEventSent(self):
+        """
+        """
+        context = self.context
+        annotations = IAnnotations(context)
+        if 'eventsent' in annotations:
+            return True
+        else:
+            return False
+
+    def canManageSite(self):
+        return checkPermission("plone.app.controlpanel.Overview", self.portal())
+
+
+class gwImportantNews(viewletBase):
+    grok.name('genweb.important')
+    grok.context(INewsItem)
+    grok.template('important')
+    grok.viewletmanager(IAboveContentTitle)
+    grok.layer(IGenwebTheme)
+
+    def permisos_important(self):
+        # TODO: Comprovar que l'usuari tingui permisos per a marcar com a important
+        return not IImportant(self.context).is_important and checkPermission("plone.app.controlpanel.Overview", self.portal())
+
+    def permisos_notimportant(self):
+        # TODO: Comprovar que l'usuari tingui permisos per a marcar com a notimportant
+        return IImportant(self.context).is_important and checkPermission("plone.app.controlpanel.Overview", self.portal())
+
+    def canManageSite(self):
+        return checkPermission("plone.app.controlpanel.Overview", self.portal())
+
+    def isNewImportant(self):
+        context = aq_inner(self.context)
+        is_important = IImportant(context).is_important
+        return is_important
