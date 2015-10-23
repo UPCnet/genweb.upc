e54207ad7fed1c9af6c929795752e8f49ea9f162
hanirok <corina.riba@upcnet.es>
2015-10-23 12:00:11 +0200

    Add replyto to contact message

--- genweb/upc/browser/contact.py
+++ genweb/upc/browser/contact.py
@@ -33,6 +33,8 @@ grok.templatedir("views_templates")
 
 
 MESSAGE_TEMPLATE = u"""\
+Reply-To: %(from_address)s
+
 Heu rebut aquest correu perquè en/na %(name)s (%(from_address)s) ha enviat \
 comentaris desde de l'espai Genweb \
 
