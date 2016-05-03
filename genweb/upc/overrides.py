ea5af4713a30e03c495e33f871d7714376237a87
Alberto Duran <alberto.duran@lpc51248.estacions.upcnet.es>
2016-05-03 16:50:35 +0200

    historial en continguts creats per un altre usuari

--- genweb/upc/overrides.py
+++ genweb/upc/overrides.py
@@ -1,6 +1,5 @@
 from plone.app.layout.viewlets.content import DocumentBylineViewlet
 from plone import api
-from Acquisition import aq_inner, aq_chain
 
 
 class DocumentBylineViewletisReader(DocumentBylineViewlet):
@@ -8,16 +7,11 @@ class DocumentBylineViewletisReader(DocumentBylineViewlet):
     def isReader(self):
         """Check if user rol is Reader"""
         userid = api.user.get_current().id
-        inner_context = aq_inner(self.context)
-        context = aq_chain(inner_context)
-        for obj in context[:-1]:
-            local_roles = obj.get_local_roles()
-            user_roles = [x for x in local_roles if x[0] == userid]
-            if user_roles:
-                roles = user_roles[0][1]
-                for rol in roles:
-                    if 'Owner' in rol or 'Editor' in rol or 'Contributor' in rol or 'Reviewer' in rol:
-                        return False
-                    else:
-                        return True
-        return True
+        user = api.user.get(username=userid)
+        roles = user.getRoles()
+        for rol in roles:
+            if rol in ['Author', 'Owner', 'Editor', 'Contributor', 'Manager', 'Reviewer', 'Site Administrator', 'WebMaster']:
+                return False
+            # Reader or Authenticated
+            else:
+                return True
