036e278ec4adec6fe0c5900fd61c13a610efef32
Alberto Duran <alberto.duran@lpc51248.estacions.upcnet.es>
2016-05-04 10:39:05 +0200

    roles in context for history viewlet

--- genweb/upc/overrides.py
+++ genweb/upc/overrides.py
@@ -8,10 +8,12 @@ class DocumentBylineViewletisReader(DocumentBylineViewlet):
         """Check if user rol is Reader"""
         userid = api.user.get_current().id
         user = api.user.get(username=userid)
-        roles = user.getRoles()
+        context = self.context
+        roles = user.getRolesInContext(context)
+        import ipdb;ipdb.set_trace()
         for rol in roles:
             if rol in ['Author', 'Owner', 'Editor', 'Contributor', 'Manager', 'Reviewer', 'Site Administrator', 'WebMaster']:
                 return False
-            # Reader or Authenticated
+            # Reader or Authenticated or Member
             else:
                 return True
