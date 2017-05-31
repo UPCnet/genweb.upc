ee29cc87991374f4213581b824ce5cb85dc1c705
Corina Riba <corina.riba@upcnet.es>
2017-05-31 10:51:09 +0200

    Change poll url

--- genweb/upc/browser/enquesta.py
+++ genweb/upc/browser/enquesta.py
@@ -21,6 +21,13 @@ class enquestaRedirect(grok.View):
     grok.require('zope2.View')
 
     def render(self):
+
+        try:
+            from plone.protect.interfaces import IDisableCSRFProtection
+            alsoProvides(self.request, IDisableCSRFProtection)
+        except:
+            pass
+
         context = aq_inner(self.context)
         if api.user.is_anonymous():
             return self.request.response.redirect(context.absolute_url())
@@ -32,7 +39,8 @@ class enquestaRedirect(grok.View):
             user_token = sha1(user_seed).hexdigest()
             logged_user.setMemberProperties(mapping={"asepeyo_hash": user_token})
 
-        enquesta_url = 'https://www.encuestafacil.com/RespWeb/Cuestionarios.aspx?EID=2254689&MT=%s' % user_token
+        enquesta_url = 'http://www.encuestafacil.com/RespWeb/Qn.aspx?EID= 2254689&ParamID=%s' % user_token
+
         return self.request.response.redirect(enquesta_url)
 
 
