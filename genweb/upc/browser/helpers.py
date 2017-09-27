9fc2142fef84471c9b19fe64f9cfd05413cc7a25
Corina Riba <corina.riba@upcnet.es>
2017-09-27 14:18:48 +0200

    Toucher for existing content old portlets

---
+++ genweb/upc/browser/helpers.py
@@ -0,0 +1,74 @@
+from zope.annotation.interfaces import IAnnotations
+from plone.portlets.interfaces import IPortletManager, IPortletRetriever
+from zope.component import getMultiAdapter, getUtility
+from genweb.upc.portlets.existing_content import IContentPortlet
+
+from plone import api
+from five import grok
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+
+class findPacketbyType(grok.View):
+    """ Return all contents of a content type """
+    grok.context(IPloneSiteRoot)
+    grok.name('find_content_bytype')
+    grok.require('cmf.ManagePortal')
+
+    def render(self):
+        items = []
+        for item in api.content.find(portal_type='packet'):
+            item_obj = item.getObject()
+            annotations = IAnnotations(item_obj)
+            item_type = annotations.get('genweb.packets.type'),
+            item_link = annotations.get("genweb.packets.url")
+            items.append('{}'.format(dict(where=item.getURL(),
+                                          packet_type=item_type,
+                                          link=item_link)))
+        return items
+
+
+class findExistingContentPortlets(grok.View):
+    grok.context(IPloneSiteRoot)
+    grok.name('find_existingcontent_portlet')
+    grok.require('cmf.ManagePortal')
+
+    def render(self):
+        portal = api.portal.get()
+        portal_ca = portal['ca']
+        portal_en = portal['en']
+        portal_es = portal['es']
+        portlets_contingut = []
+        portlets = ()
+        for portal_xx in (portal_ca['benvingut'], portal_en['welcome'], portal_es['bienvenido']):
+            for column in ["genweb.portlets.HomePortletManager1",
+                           "genweb.portlets.HomePortletManager2",
+                           "genweb.portlets.HomePortletManager3",
+                           "genweb.portlets.HomePortletManager4",
+                           "genweb.portlets.HomePortletManager5",
+                           "genweb.portlets.HomePortletManager6",
+                           "genweb.portlets.HomePortletManager7",
+                           "genweb.portlets.HomePortletManager8",
+                           "genweb.portlets.HomePortletManager9",
+                           "genweb.portlets.HomePortletManager10"
+                           ]:
+                            # "plone.leftcolumn",
+                            # "plone.rightcolumn"
+                manager = getUtility(IPortletManager, name=column, context=portal_xx)
+                retriever = getMultiAdapter((portal_xx, manager), IPortletRetriever)
+                portlets = retriever.getPortlets()
+                if portlets:
+                    for portlet in portlets:
+                        # portlet is {'category': 'context', 'assignment': <FacebookLikeBoxAssignment at facebook-like-box>, 'name': u'facebook-like-box', 'key': '/isleofback/sisalto/huvit-ja-harrasteet
+                        # Identify portlet by interface provided by assignment
+                        if IContentPortlet.providedBy(portlet["assignment"]):
+                            portlets_contingut.append('{}'.format(dict(where=portal_xx.title,
+                                                                       column=column,
+                                                                       portlet=portlet["name"],
+                                                                       link=portlet["assignment"].url
+                                                                       )
+                                                                  )
+                                                      )
+        if portlets_contingut:
+            return portlets_contingut
+        else:
+            return 'No hi ha portlets'
