325a1a8a28911bd12d4dcfffaf00d66419f81bee
Iago López Fernández <iago.lopez@upcnet.es>
2021-02-23 13:09:39 +0100

    Meta tags

--- genweb/upc/browser/social.py
+++ genweb/upc/browser/social.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
-from Products.CMFPlone.interfaces.syndication import IFeedItem
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.browser.syndication.adapters import FolderFeed, BaseItem
+from Products.CMFPlone.interfaces.syndication import IFeedItem
 
 from plone import api
 from plone.app.layout.viewlets.common import TitleViewlet
+from plone.registry.interfaces import IRegistry
 
+from zope.component import getUtility
 from zope.component import queryMultiAdapter
 from zope.component.hooks import getSite
+
+from genweb.controlpanel.interface import IGenwebControlPanelSettings
 
 
 class SocialTagsTwitterViewlet(TitleViewlet):
@@ -18,7 +23,15 @@ class SocialTagsTwitterViewlet(TitleViewlet):
         if item is None:
             item = BaseItem(self.context, feed)
 
-        tags = {"title": item.title,
+        registry = getUtility(IRegistry)
+        gwsettings = registry.forInterface(IGenwebControlPanelSettings)
+
+        lang = getToolByName(api.portal.get(), 'portal_languages').getPreferredLanguage()
+        if not lang:
+            lang = 'ca'
+
+        tags = {"site": getattr(gwsettings, 'html_title_{}'.format(lang)),
+                "title": item.title,
                 "description": item.description,
                 "url": item.link,
                 "image": None}
