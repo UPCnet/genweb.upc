d9224f04d9ecaa7b748e42d785c7857cd5982bdd
alberto.duran <alberto.duran@upcnet.es>
2021-03-17 11:16:38 +0100

    New feature for download files and images as folder structure in zip file

---
+++ genweb/upc/browser/folders.py
@@ -0,0 +1,138 @@
+import os
+import transaction
+from datetime import datetime
+from plone import api
+from plone.namedfile import NamedBlobFile
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.statusmessages.interfaces import IStatusMessage
+
+
+class DownloadFiles(BrowserView):
+
+    template = ViewPageTemplateFile('views_templates/download_files.pt')
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+    
+    def __call__(self):
+        form = self.request.form
+        if not form or 'file_type' not in form:
+            return self.template()
+
+        items = query_items_in_natural_sort_order(self.context, {'portal_type' : ('File', 'Image')})
+        if not items:
+            IStatusMessage(self.request).addStatusMessage(u"No files found!", "info")
+            return self.template()
+        
+        today = datetime.today().strftime("%Y-%m-%d")
+        plone_id = 'export-{0}'.format(self.context.id)
+        exp_path = 'export-{0}-{1}'.format(self.context.id, today)
+
+        if os.path.exists(exp_path):
+            os.system('rm -rf {}'.format(exp_path))
+        if plone_id in self.context:
+            api.content.delete(obj=self.context[plone_id])
+
+        items = query_items_in_natural_sort_order(self.context, {'portal_type' : ('Folder', 'File', 'Image')})
+
+        os.mkdir(exp_path)
+        from_path = '/'.join(self.context.getPhysicalPath())
+        folders = {
+            plone_id: from_path
+        }
+
+        for item in items:
+            relative_path = os.path.relpath(item.getPath(), from_path) # diff between item path and root path
+            zip_path = os.path.join(exp_path, relative_path)
+            if item.portal_type == 'Folder':
+                os.mkdir(zip_path) # create folder in root path + relative path
+                folders.update({item.id.lower(): item.getPath()}) # update virtual folder structure
+                print("Saved {}".format(zip_path))
+            elif item.portal_type == 'File':
+                obj = item.getObject()
+                for x in folders:
+                    test_path = folders[x] + '/' + obj.id
+                    if test_path == item.getPath():
+                        f = open(zip_path, 'wb')
+                f.write(obj.file.data)
+                f.close()
+                print("Saved {}".format(zip_path))
+            elif item.portal_type == 'Image':
+                obj = item.getObject()
+                for x in folders:
+                    test_path = folders[x] + '/' + obj.id
+                    if test_path == item.getPath():
+                        f = open(zip_path, 'wb')
+                f.write(obj.image.data)
+                f.close()
+                print("Saved {}".format(zip_path))
+
+        os.system('zip -r {0}.zip {0}'.format(exp_path))
+        os.system('rm -rf {}'.format(exp_path))
+        zip_file = api.content.create(
+            type='File',
+            title=exp_path,
+            id=plone_id,
+            container=self.context,
+        )
+        zip_file.file = NamedBlobFile(
+            data=open('{}.zip'.format(exp_path), 'r'),
+            filename=u'{}.zip'.format(exp_path),
+            contentType='application/zip'
+        )
+        zip_file.reindexObject()
+        transaction.commit()
+        self.request.response.redirect(zip_file.absolute_url() + '/view')
+
+
+from plone.app.layout.navigation.navtree import buildFolderTree
+from plone.app.layout.navigation.navtree import NavtreeStrategyBase
+# https://github.com/plone/Products.CMFPlone/blob/master/Products/CMFPlone/browser/navtree.py
+from Products.CMFPlone.browser.navtree import SitemapNavtreeStrategy, DefaultNavtreeStrategy
+
+def query_items_in_natural_sort_order(root, query):
+    """
+    Create a flattened out list of portal_catalog queried items in their natural depth first navigation order.
+
+    @param root: Content item which acts as a navigation root
+
+    @param query: Dictionary of portal_catalog query parameters
+
+    @return: List of catalog brains
+    """
+
+    # Navigation tree base portal_catalog query parameters
+    applied_query=  {
+        'path' : '/'.join(root.getPhysicalPath()),
+        'sort_on' : 'getObjPositionInParent'
+    }
+
+    # Apply caller's filters
+    applied_query.update(query)
+    
+    # Set the navigation tree build strategy
+    # - use navigation portlet strategy as base
+    strategy = DefaultNavtreeStrategy(root)
+    strategy.rootPath = '/'.join(root.getPhysicalPath())
+    strategy.showAllParents = False
+    strategy.bottomLevel = 999
+    # This will yield out tree of nested dicts of
+    # item brains with retrofitted navigational data
+    tree = buildFolderTree(root, root, applied_query, strategy=strategy)
+
+    items = []
+
+    def flatten(children):
+        """ Recursively flatten the tree """
+        for c in children:
+            # Copy catalog brain object into the result
+            items.append(c["item"])
+            children = c.get("children", None)
+            if children:
+                flatten(children)
+
+    flatten(tree["children"])
+
+    return items
