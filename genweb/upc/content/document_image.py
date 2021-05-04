e91227f1257f86c476da9b57dd80ecb3e9e3b570
Iago López Fernández <iago.lopez@upcnet.es>
2021-05-04 15:18:02 +0200

    Añadir check para quitar imagen de /view en el contenido Página con imagen

--- genweb/upc/content/document_image.py
+++ genweb/upc/content/document_image.py
@@ -3,14 +3,36 @@ from AccessControl.SecurityInfo import ClassSecurityInfo
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from five import grok
+from plone.app.contenttypes import _
 from plone.directives import form
 from plone.dexterity.content import Item
+from plone.namedfile import field as namedfile
+from zope import schema
 from zope.interface import implements
+
+from genweb.core import GenwebMessageFactory as _GwC
 
 
 class IDocumentImage(form.Schema):
     """
     """
+    image = namedfile.NamedBlobImage(
+        title=_(u"Lead Image"),
+        description=u"",
+        required=True,
+    )
+
+    not_show_image = schema.Bool(
+        title=_GwC(u"not_show_image"),
+        description=u"",
+        required=False,
+    )
+
+    image_caption = schema.TextLine(
+        title=_(u"Lead Image Caption"),
+        description=u"",
+        required=False,
+    )
 
 
 class DocumentImage(Item):
