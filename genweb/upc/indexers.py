ec66de9dfcb5396a193836f586d21325a6e8f857
Iago Lopez <iago.lopez@pc68632.estacions.upcnet.es>
2022-01-10 09:40:17 +0100

    Quitar texto del searchabletext de los documentos con imagen

--- genweb/upc/indexers.py
+++ genweb/upc/indexers.py
@@ -5,37 +5,38 @@ from Products.PlonePAS.utils import safe_unicode
 from plone.indexer.decorator import indexer
 from plone.app.contenttypes.indexers import _unicode_save_string_concat
 
+from genweb.core.patches import SearchableText
 from genweb.upc.content.document_image import IDocumentImage
 
 
 # Add subjects and creators to searchableText Dexterity objects
-def SearchableText(obj, text=False):
-    subjList = []
-    creatorList = []
+# def SearchableText(obj, text=False):
+#     subjList = []
+#     creatorList = []
 
-    for sub in obj.subject:
-        subjList.append(sub)
-    subjects = ','.join(subjList)
+#     for sub in obj.subject:
+#         subjList.append(sub)
+#     subjects = ','.join(subjList)
 
-    for creator in obj.creators:
-        creatorList.append(creator)
-    creators = ','.join(creatorList)
+#     for creator in obj.creators:
+#         creatorList.append(creator)
+#     creators = ','.join(creatorList)
 
-    text = ''
-    try:
-        if obj.text and obj.text.output:
-            text = BeautifulSoup(obj.text.output).getText()
-    except:
-        text = ''
+#     text = ''
+#     try:
+#         if obj.text and obj.text.output:
+#             text = BeautifulSoup(obj.text.output).getText()
+#     except:
+#         text = ''
 
-    return u' '.join((
-        safe_unicode(obj.id),
-        safe_unicode(obj.title) or u'',
-        safe_unicode(obj.description) or u'',
-        safe_unicode(subjects) or u'',
-        safe_unicode(creators) or u'',
-        safe_unicode(text) or u'',
-    ))
+#     return u' '.join((
+#         safe_unicode(obj.id),
+#         safe_unicode(obj.title) or u'',
+#         safe_unicode(obj.description) or u'',
+#         safe_unicode(subjects) or u'',
+#         safe_unicode(creators) or u'',
+#         safe_unicode(text) or u'',
+#     ))
 
 
 @indexer(IDocumentImage)
